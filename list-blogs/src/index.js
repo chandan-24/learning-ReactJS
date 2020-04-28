8a0fd0da587d94b380cf952509b1e7f44f72e6fc
Chandan Kumar <chandan.kumar9503@gmail.com>
2020-04-28 21:00:44 +0530

    entry point of the webapp

--- list-blogs/src/index.js
+++ list-blogs/src/index.js
@@ -2,14 +2,18 @@ const React = require('react');
 const ReactDom = require('react-dom');
 const reactRedux = require('react-redux');
 const redux = require('redux');
+const thunk = require('redux-thunk').default;
 
 const reducers = require('./reducers');
 const App = require('./components/App');
 const Provider = reactRedux.Provider;
 const createStore = redux.createStore;
+const applyMiddleware = redux.applyMiddleware;
+
+const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDom.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.querySelector('#root')
